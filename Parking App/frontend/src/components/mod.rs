ee46206e1efbf2288b66514bb53ec8fd2da42177
Saltycaker <142301886+saltycaker@users.noreply.github.com>
2026-08-16 10:23:08 -0700

    Delete Parking App directory

--- Parking App/frontend/src/components/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod layout;
-pub mod map;
-
-pub use layout::*;
-pub use map::*;
--- Parking App/frontend/src/pages/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod home;
-pub mod parking_detail;
-pub mod navigation;
-pub mod profile;
-
-pub use home::Home;
-pub use parking_detail::ParkingDetail;
-pub use navigation::Navigation;
-pub use profile::Profile;
